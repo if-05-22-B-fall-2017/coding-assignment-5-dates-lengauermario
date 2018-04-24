d16938d73c46f8cd1254af4b87983e04d1a59195
lengauermario <mario.lengauer@gmx.net>
2018-04-24 16:52:15 +0200

    regex add

--- src/main/java/GermanStyleDateChecker.java
+++ src/main/java/GermanStyleDateChecker.java
@@ -2,7 +2,8 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class GermanStyleDateChecker {
+    private static final Pattern pattern = Pattern.compile("^([12]?\\d|3[01]|[1-9])[. :\\-](0?[1-9]|1[0-2])[. :\\-](19|20)?\\d{2}$");
     public static Matcher getMatcher(String dateString) {
-        return null;
+        return pattern.matcher(dateString);
     }
 }
